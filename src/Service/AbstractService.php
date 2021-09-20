770e8e0cd12b2b8d6a2a32f7d16199a347b5b019
mavrin_88 <mavrin_88@mail.ru>
2021-09-20 15:38:07 +0300

    Update YaMetrika class and add AbstractService class

---
+++ src/Service/AbstractService.php
@@ -0,0 +1,30 @@
+<?php
+
+use API\YaMetrika\Exceptions\YaMetrikaException;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+
+class AbstractService
+{
+    /**
+     * Send Request
+     *
+     * @param $url
+     *
+     * @return array
+     * @throws YaMetrikaException
+     */
+    private function query($url)
+    {
+        try {
+            $client = new GuzzleClient($this->getHttpClientParams());
+            $response = $client->request('GET', $url);
+            $result = json_decode($response->getBody(), true);
+
+            return $result;
+
+        } catch (ClientException $e) {
+            throw new YaMetrikaException($e->getMessage());
+        }
+    }
+}
--- src/Service/V1/YaMetrika.php
+++ src/Service/V1/YaMetrika.php
@@ -3,6 +3,7 @@
 namespace API\YaMetrika;
 
 use API\YaMetrika\Exceptions\YaMetrikaException;
+use Carbon\Carbon;
 
 /**
  * Class YaMetrika
@@ -11,7 +12,52 @@ use API\YaMetrika\Exceptions\YaMetrikaException;
  * @link    https://github.com/mavrin88/YandexMetrika
  * @package API\YaMetrika
  */
-class YaMetrika
+class YaMetrika extends \AbstractService
 {
+    /**
+     * URL API Yandex Metrika
+     *
+     * @var string
+     */
+    private $endPoint = 'https://api-metrika.yandex.ru/stat/v1/data';
 
+    /**
+     * Token API
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Id counter
+     *
+     * @var int
+     */
+    private $counterId;
+
+    public function __construct($token, $counterId)
+    {
+        $this->token = $token;
+        $this->counterId = $counterId;
+    }
+
+    /**
+     * Calculate the end and start date.
+     *
+     * @param $amountOfDays
+     *
+     * @return array
+     */
+    private function differenceDate($amountOfDays)
+    {
+        $startDate = Carbon::today()->subDays($amountOfDays);
+        $endDate = Carbon::today();
+
+        return [$startDate, $endDate];
+    }
+
+    public function creataLink($params)
+    {
+        return $this->endPoint . '?' . http_build_query(array_merge($params, ['ids' => $this->counterId]), null, '&');
+    }
 }
