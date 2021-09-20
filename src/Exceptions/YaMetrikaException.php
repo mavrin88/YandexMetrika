5a8ecc3ef8d82f7befed0c2bad3b0ce297106229
mavrin_88 <mavrin_88@mail.ru>
2021-09-20 15:02:39 +0300

    Add YaMetrika class

---
+++ src/Exceptions/YaMetrikaException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace API\YaMetrika\Exceptions;
+
+use Exception;
+
+/**
+ * Class YaMetrikaException
+ *
+ * @author  Alexey Marchenko <mavrin_88@mail.ru>
+ * @package API\YaMetrika\Exceptions
+ */
+class YaMetrikaException extends Exception
+{
+
+}
---
+++ src/Service/V1/YaMetrika.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace API\YaMetrika;
+
+use API\YaMetrika\Exceptions\YaMetrikaException;
+
+/**
+ * Class YaMetrika
+ *
+ * @author  Alexey Marchenko <mavrin_88@mail.ru>
+ * @link    https://github.com/mavrin88/YandexMetrika
+ * @package API\YaMetrika
+ */
+class YaMetrika
+{
+
+}
